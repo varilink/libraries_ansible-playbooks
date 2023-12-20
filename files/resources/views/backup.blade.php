cc11e830eec34d3816a0b65942e4596d90f202c1
David Williamson <david.williamson@varilink.co.uk>
2023-12-20 14:02:10 +0000

    VERY DRAFT IMPLEMENTATION OF THE SERVICES PORTAL
    This is going to need a lot more work so I am committing it where it's
    at currently so that I can pull it into the services-docker repository
    to develop and test it further there.

---
+++ files/resources/views/backup.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+	<x-slot name="title">
+		Backup
+	</x-slot>
+
+	<div id="content">
+<iframe src="https://portal.varilink.co.uk/bacula/" width="100%" height="100%" title="Backup"></iframe>
+</div>
+
+</x-layout>
---
+++ files/resources/views/monitor.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+	<x-slot name="title">
+		Monitor
+	</x-slot>
+
+	<div id="content">
+<iframe src="https://portal.varilink.co.uk/grafana/" width="100%" height="100%" title="Monitor"></iframe>
+</div>
+
+</x-layout>
